ee772c292bb73050f87f0990a2dde75df36cfedc
Antonio Hernández Cervantes <hernandezcer@gmail.com>
2025-09-11 06:41:05 +0200

    feat: add ellipsis to long task links

--- components/LinkifiedText/LinkifiedText.tsx
+++ components/LinkifiedText/LinkifiedText.tsx
@@ -15,7 +15,8 @@ export default function LinkifiedText({ text }: LinkifiedTextProps) {
             href={part}
             target="_blank"
             rel="noreferrer noopener"
-            className="underline text-blue-600 dark:text-blue-400 block max-w-full overflow-x-auto whitespace-nowrap [scrollbar-width:thin] [&::-webkit-scrollbar]:h-1 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:rounded [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-thumb]:bg-gray-600"
+            title={part}
+            className="underline text-blue-600 dark:text-blue-400 block max-w-full truncate"
             onClick={e => e.stopPropagation()}
           >
             {part}
